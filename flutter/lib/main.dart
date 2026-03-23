60560300c2b2bb9780abaf21ee50ab7c549f5612
hjw1986 <119439549+hjw1986@users.noreply.github.com>
2026-03-23 10:08:07 +0800

    Update main.dart

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -293,7 +293,8 @@ void runConnectionManagerScreen() async {
     const DesktopServerPage(),
     MyTheme.currentThemeMode(),
   );
-  final hide = await bind.cmGetConfig(name: "hide_cm") == 'true';
+  
+final hide = await bind.cmGetConfig(name: "hide_cm") == 'true';
   gFFI.serverModel.hideCm = hide;
   if (hide) {
     await hideCmWindow(isStartup: true);
